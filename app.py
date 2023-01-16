66f835764e5349e0e01cdef78e7cd9dd723b0bd0
yanJuicy <hanjub3@gmail.com>
2023-01-16 16:35:59 +0900

    도커 3주차

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 
-app = Flask(__name__)
+from utils import get_movie_info
 
-import requests
-from bs4 import BeautifulSoup
+app = Flask(__name__)
 
 from pymongo import MongoClient
 
@@ -29,16 +28,7 @@ def saving():
     url_receive = request.form['url_give']
     comment_receive = request.form['comment_give']
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    title = soup.select_one('meta[property="og:title"]')['content']
-    image = soup.select_one('meta[property="og:image"]')['content']
-    desc = soup.select_one('meta[property="og:description"]')['content']
-
+    title, image, desc = get_movie_info(url_receive)
     doc = {
         'title': title,
         'image': image,
